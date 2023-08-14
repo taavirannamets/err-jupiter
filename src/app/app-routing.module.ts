1eb02447bc7333e3d82fda39d0994ceb46912b64
taavirannamets <taavirannamets@gmail.com>
2023-08-14 16:12:32 +0300

    created landing page c and m + added to routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './video/feature/landing-page/landing-page/landing-page.component';
 
-const routes: Routes = [{ path: '', redirectTo: 'video', pathMatch: 'full' }];
+const routes: Routes = [
+  { path: '', redirectTo: 'video', pathMatch: 'full' },
+  { path: 'video', component: LandingPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
